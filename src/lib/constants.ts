138e94b8c427fc3288ccf8b34872910d4ee9be3a
Cristian F. Torres Collazo <cristian@inprende.com>
2026-03-04 12:48:59 -0400

    fix: update contact email to the correct address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const CONTACT_EMAIL = 'cristianf.torres15@gmail.com'
+export const CONTACT_EMAIL = 'mac.educando@gmail.com'
 export const WEB3FORMS_KEY = import.meta.env.VITE_WEB3FORMS_KEY as string
